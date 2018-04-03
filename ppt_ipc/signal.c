be2d5aa249b79bb8692ac4ad018c4cb954f8b9ec
newiyuil <newiyuil@163.com>
2018-04-03 23:28:57 +0800

    update ./ppt_ipc/signal.c

--- ppt_ipc/signal.c
+++ ppt_ipc/signal.c
@@ -6,30 +6,32 @@
 ************************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <signal.h>
 #include <error.h>
 
 typedef void (*sighandler_t)(int);
 
 static void SIGKIL_HANDLER(int signo) {
-    if (signo == SIGKILL) {
+    if (signo == SIGINT) {
         printf("get signal kill\n");
     } else {
         printf("get signal %d\n", signo);
     }
+    exit(signo);
 }
 
 int main()
 {
     sighandler_t ret;
-    ret = signal(SIGSTOP, SIGKIL_HANDLER);
+    ret = signal(SIGINT, SIGKIL_HANDLER);
     if (ret == SIG_ERR) {
         fprintf(stderr, "mount SIGSTOP handler error\t");
         perror("cause");
         return -1;
     }
 
-    raise(SIGKILL);
+    raise(SIGINT);
 
     return 0;
 }
